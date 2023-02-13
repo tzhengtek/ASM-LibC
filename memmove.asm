1160c072b129f5c836d0ea37ffcd336eec5cfaeb
Timothé <timothe.zheng@epitech.eu>
2023-02-13 09:55:31 +0100

    FIX: changes condition for forward and backward

--- memmove.asm
+++ memmove.asm
@@ -8,19 +8,21 @@ memmove:
         XOR RCX, RCX                    ; Init RCX to 0
         XOR R8, R8                      ; Init R8 to 0
         CMP RDI, RSI                    ; Check pointer of Src and Dest
-        JLE loop_forward                ; If greater: Jump on loop forward
+        JL loop_forward                ; If greater: Jump on loop forward
         JMP launch_backward              ; If lower: Jump on launch backward
 
 launch_backward:
-        DEC RDX                         ; Decrease by one the n byte
+        MOV RCX, RDX                    ; Save RDX in RCX
+        DEC RCX                         ; Decrease by one the n byte
         JMP loop_backward               ; Jump on loop backward
 
 loop_backward:
-        MOV R8B, [RSI + RDX]            ; Save in register the nb byte on RSI
-        MOV [RDI + RDX], R8B            ; Move the save byte into RDI
-        CMP RDX, 0                      ; Check if nb byte is 0
+        XOR R8, R8
+        MOV R8B, [RSI + RCX]            ; Save in register the nb byte on RSI
+        MOV [RDI + RCX], R8B            ; Move the save byte into RDI
+        CMP RCX, 0                      ; Check if nb byte is 0
         JE end                          ; End loop
-        DEC RDX                         ; Decrease by one
+        DEC RCX                         ; Decrease by one
         JMP loop_backward               ; Restart the loop
 
 loop_forward:
